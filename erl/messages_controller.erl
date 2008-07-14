9c7237a050d38db06f05245e8c7cc725ed31ef49
John Andrews <john.andrews@getbraintree.com>
2008-07-14 10:47:22 -0400

    rudimentary polling http client. uses jquery. oh did i mention it fucking rules? yes, it does.

--- erl/messages_controller.erl
+++ erl/messages_controller.erl
@@ -18,10 +18,18 @@ handle_request('GET', [], _) ->
     H2 = "All Rooms",
     format_list(H2, MsgList);
 
-handle_request('GET', [Room|_], _) ->
-    MsgList = message:all(Room, limit, 3),
-    H2 = "In Room: " ++ Room,
-    format_list(H2, MsgList);
+handle_request('GET', [Room|_], Arg) ->
+    case yaws_api:queryvar(Arg, "newer_than") of
+        {ok, I}  -> 
+            case string:to_integer(I) of
+                {error, _} -> MsgList = [];
+                {ID, _}    -> MsgList = message:all(Room, newer_than, ID)
+            end;
+        _ -> MsgList = message:all(Room, limit, 30)
+    end,
+%    H2 = "In Room: " ++ Room,
+    messages_json(MsgList);
+%    format_list(H2, MsgList);
 
 handle_request('POST', [Room|_], Arg) ->
     {ok, Auth} = yaws_api:postvar(Arg, "author"),
@@ -29,12 +37,20 @@ handle_request('POST', [Room|_], Arg) ->
     {ok, Type} = yaws_api:postvar(Arg, "type"),
     io:format("Author: ~p~nType: ~p~nBody: ~p~n", [Auth, Type, Body]),
     message:create(Room, Type, Auth, Body),
-    [{status, 200}].
+    [{status, 200}];
+
+handle_request(_, _, _Arg) -> % catchall
+    [{status, 501}].
 
 format_list(H2, MsgList) ->
     layout([{h1, [], "Messages"},
             {h2, [], H2},
             {ul, [], messages_list(MsgList)}]).
+
+messages_json(List) ->
+%    MJ = [ {struct, [{"author",A}, {"type",T}, {"body",B}] }  || {_, _, _, T, A, B, _Timestamp} <- List],
+    MJ = [ {struct, [{"author",A}, {"body",B}, {"id", ID}] }  || {_, ID, _Room, _Type, A, B, _Timestamp} <- List],
+    {html, json:encode({array, MJ})}.
 
 messages_list(List) ->
     [{li, [], Auth ++ ": " ++ Msg} || {_, _, _, _, Auth, Msg, _} <- List].
--- test.rb
+++ test.rb
@@ -3,8 +3,14 @@
 require 'net/http'
 require 'uri'
 
+#srv = Net::HTTP.new('localhost'
+
 res = Net::HTTP.post_form(URI.parse('http://localhost:8899/messages/braintree/'),
                               {'author' => 'xandrews', 'body' => ARGV[0], 'type' => 'msg'})
 
 p res
 puts res.body
+
+del = Net::HTTP.new('localhost', 8899).delete('/messages/braintree')
+p del
+p del.body
