0f5877e1617dbcf58caac53d5ce52a7b8e279fff
John Andrews <john.andrews@getbraintree.com>
2008-08-22 22:38:38 -0400

    crudely check the cookie

--- src/api_controller.erl
+++ src/api_controller.erl
@@ -7,9 +7,19 @@ out(A) ->
     Req = A#arg.req,
     Method = Req#http_request.method,
     Path = string:tokens(A#arg.appmoddata, "/"),
-%%    io:format("~p - ~p~n",[Method, Req#http_request.path]),
+    io:format("Cookie: ~p~n",[get_cookie(A)]),
     case Path of
         ["rooms"|_] -> rooms_controller:out(A);
         ["messages"|Tail] -> messages_controller:handle_request(Method, Tail, A);
         _ -> [{status, 404}]
+    end.
+
+get_cookie(A) ->
+    H = A#arg.headers,
+    C = H#headers.cookie,
+    case yaws_api:find_cookie_val("watercooler-cred", C) of
+        [] -> none;
+        Cookie ->
+            {ok, OP} = yaws_api:cookieval_to_opaque(Cookie),
+            {OP, Cookie}
     end.
